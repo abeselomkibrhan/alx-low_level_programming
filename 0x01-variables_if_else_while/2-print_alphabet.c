9e8d7591c671ac79437950353f75ee2da8f5d783
abeselomkibrhan <abikibhan@gmail.com>
2023-06-17 09:55:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	/* your code goes there */
+	int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z'){
+putchar(lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
+}
